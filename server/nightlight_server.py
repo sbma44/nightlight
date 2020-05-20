b4433c818fadb294798193ec7bc3843ae87ef38b
Tom Lee <thomas.j.lee@gmail.com>
2020-05-19 23:26:01 -0400

    move to mqtt, add systemd script

--- server/nightlight_server.py
+++ server/nightlight_server.py
@@ -1,64 +1,78 @@
-import json
+import struct
+import time
 import math
 import datetime
-from http.server import BaseHTTPRequestHandler
-from urllib import parse
+import paho.mqtt.client as mqtt
 
+TOPIC = 'nightlight'
+SESSION_LENGTH = 30
+FADEOUT = 5
+TIC_INCREMENT = 2.0
+MAX_VALUE = 1023
 
-class GetHandler(BaseHTTPRequestHandler):
+global current_session_end
+current_session_end = -1
+global last_value
+last_value = -1
 
-    def do_GET(self):
-        parsed_path = parse.urlparse(self.path)
-        message_parts = [
-            'CLIENT VALUES:',
-            'client_address={} ({})'.format(
-                self.client_address,
-                self.address_string()),
-            'command={}'.format(self.command),
-            'path={}'.format(self.path),
-            'real path={}'.format(parsed_path.path),
-            'query={}'.format(parsed_path.query),
-            'request_version={}'.format(self.request_version),
-            '',
-            'SERVER VALUES:',
-            'server_version={}'.format(self.server_version),
-            'sys_version={}'.format(self.sys_version),
-            'protocol_version={}'.format(self.protocol_version),
-            '',
-            'HEADERS RECEIVED:',
-        ]
-        for name, value in sorted(self.headers.items()):
-            message_parts.append(
-                '{}={}'.format(name, value.rstrip())
-            )
-        message_parts.append('')
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
 
-        FADE_OUT = 5
-        MAX_VAL = 1023
-        d = datetime.datetime.now()
-        hour = d.hour
-        minute = d.minute
-        if hour == 20:
-            if minute < 50:
-                val = MAX_VAL
-            else:
-                val = math.floor(((FADE_OUT - (minute - FADE_OUT)) / (FADE_OUT * 1.0)) * MAX_VAL)
-        elif hour < 20:
-            val = 0 # don't show anything but keep querying
-        elif hour >= 21:
-            val = -1 # sleep until reset
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe(TOPIC)
 
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    global current_session_end
+    global last_value
 
-        message = json.dumps({'val' : int(val)})
-        self.send_response(200)
-        self.send_header('Content-Type',
-                         'application/json; charset=utf-8')
-        self.end_headers()
-        self.wfile.write(message.encode('utf-8'))
+    print('[{}] {}'.format(msg.topic, str(msg.payload)))
 
+    if msg.topic != TOPIC:
+        return
 
-if __name__ == '__main__':
-    from http.server import HTTPServer
-    server = HTTPServer(('0.0.0.0', 8331), GetHandler)
-    print('Starting server, use <Ctrl-C> to stop')
-    server.serve_forever()
+    if msg.payload == b'start':
+        # no late night reading!
+        hour = datetime.datetime.now().hour
+        if hour <= 20 and hour >= 7:
+            current_session_end = time.time() + (SESSION_LENGTH * 60)
+            last_value = -99 # force send of new value
+            print('starting new session, ends at {}'.format(current_session_end))
+        else:
+            # turn off immediately
+            print('forbidden hours, sending shutdown')
+            current_session_end = time.time() - 1
+            last_value = -99
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("127.0.0.1", 1883, 60)
+client.loop_start()
+
+while True:
+    if current_session_end < 0:
+        val = None
+    elif current_session_end < time.time():
+        val = -1
+    else:
+        remaining = (current_session_end - time.time()) / 60.0
+        if remaining > FADEOUT:
+            val = MAX_VALUE
+        else:
+            val = math.floor((remaining / (1.0 * FADEOUT)) * MAX_VALUE)
+
+    if val is not None and val != last_value:
+        val = min(val, MAX_VALUE)
+
+        if val == -1:
+            print('session ended, putting nightlight to sleep')
+
+        last_value = val
+        print('sending {} to {}'.format(val, TOPIC))
+        client.publish(TOPIC, struct.pack('>i', val))
+
+    time.sleep(TIC_INCREMENT)
