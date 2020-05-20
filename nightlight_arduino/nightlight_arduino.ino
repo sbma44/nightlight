c58d0eb48f2fa008d51547dd65e757eb00434ae5
Tom Lee <thomas.j.lee@gmail.com>
2020-05-19 23:27:32 -0400

    fix pin; move to mqtt

--- nightlight_arduino/nightlight_arduino.ino
+++ nightlight_arduino/nightlight_arduino.ino
@@ -1,27 +1,78 @@
-#include <ArduinoJson.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
+#include <PubSubClient.h>
 #include "wifi_credentials.h"
 
-#define LED_PIN D6
+#define LED_PIN D8
 #define MAX_BRIGHTNESS 1023
 
 #define DEFAULT_DURATION 1800000
 #define FADE_DURATION 300000
 
-#define SERVER_IP "192.168.1.2"
-#define SERVER_PORT "8331"
+const char* MQTT_IP = "192.168.1.2";
 
-HTTPClient http;
-const int capacity = JSON_OBJECT_SIZE(3) + 2*JSON_OBJECT_SIZE(1);
-StaticJsonDocument<capacity> doc;
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("]");
+  for (int i = 0; i < length; i++) {
+    Serial.print(" 0x");
+    Serial.print(String(payload[i], HEX));
+  }
+  Serial.println();
+
+  int val;
+  if ((payload[0] == 0xFF) && (payload[1] == 0xFF) && (payload[2] == 0xFF) && (payload[3] == 0xFF)) {
+    ESP.deepSleep(0);
+  }
+  else {
+    val = (payload[2] * 256) + payload[3];
+    if (val > MAX_BRIGHTNESS)
+      val = MAX_BRIGHTNESS;
+    if (val < 0)
+      val = 0;
+    analogWrite(LED_PIN, val);
+  }
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "Nightlight-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      client.publish("nightlight", "start");
+      // ... and resubscribe
+      client.subscribe("nightlight");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
 
 void setup()
 {
+  pinMode(LED_PIN, OUTPUT);
+  analogWrite(LED_PIN, 0);
+
   Serial.begin(115200);
   Serial.println();
 
   WiFi.begin(WIFI_SSID, WIFI_PSK);
+
+  client.setServer(MQTT_IP, 1883);
+  client.setCallback(callback);
 
   Serial.print("Connecting");
   unsigned long start = millis();
@@ -53,38 +104,9 @@ void loop() {
     analogWrite(LED_PIN, val);
   }
   else {
-    char url[50];
-    sprintf(url, "http://%s:%s/status", SERVER_IP, SERVER_PORT);
-    Serial.print("Querying ");
-    Serial.println(url);
-    
-    http.begin(url);
-    int httpCode = http.GET();
-    if (httpCode == 200) {
-      String payload = http.getString();  
-      Serial.println(payload);
-      
-      DeserializationError err = deserializeJson(doc, payload);
-      if (err) {
-        Serial.println("deserialization failed");
-      }
-      else {
-        long val = doc["val"].as<long>();
-        if (val == -1) {
-          ESP.deepSleep(0);
-        }
-        if (val > MAX_BRIGHTNESS)
-          val = MAX_BRIGHTNESS;
-        if (val < 0)
-          val = 0;
-        analogWrite(LED_PIN, val);
-      }
+    if (!client.connected()) {
+      reconnect();
     }
-    else {
-      Serial.print("HTTP error: ");
-      Serial.println(httpCode);
-    }
-    http.end();
-    delay(10000);
+    client.loop();
   }
 }
